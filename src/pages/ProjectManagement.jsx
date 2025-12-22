95c0fb6b0bfccdc95377d02125521ced3176b421
davinder chawla <dav4uonly@gmail.com>
2025-12-22 09:09:54 +0000

    File changes

--- src/pages/ProjectManagement.jsx
+++ src/pages/ProjectManagement.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
-import { useQuery, useMutation, useQueryClient } from "@tantml:react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { format } from "date-fns";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
